8bfe2fcc5b18a24cb9a1987a14088a31c9038d62
Matthew Eldredge <mattheweldredge@gmail.com>
2017-12-18 15:02:43 -0700

    step 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { Provider } from "react-redux";
+import store from "./store.js";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>, document.getElementById('root'));
 registerServiceWorker();
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 
-import { reducer } from "./ducks/reducer.js";
+import reducer from "./ducks/reducer.js";
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
